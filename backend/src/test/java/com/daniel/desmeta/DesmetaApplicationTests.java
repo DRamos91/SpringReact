e49bb1618c84ed464219cf15d147f8811be61d29
Daniel <danielmrg@gmail.com>
2022-09-07 16:12:34 -0300

    Project created

---
+++ backend/src/test/java/com/daniel/desmeta/DesmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.desmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
